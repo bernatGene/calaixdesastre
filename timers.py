5e423534464aa0e95831c07752877c7f2f0db7d7
Bernat Skrabec <bernatgene@gmail.com>
2023-07-15 19:41:15 +0200

    presets

--- timers.py
+++ timers.py
@@ -9,11 +9,11 @@ async def set_timer(dt):
     te = t0 + dt
     while (rem := (te - time.time())) > 0:
         rem = int(round(rem))
-        await asyncio.sleep(1)
         h = rem // (60**2)
         m = (rem % (60**2)) // 60
         s = rem - (h * 60**2 + m * 60)
         print(f"Time left: {h:02d}:{m:02d}:{s:02d}", end="\r")
+        await asyncio.sleep(1)
     playsound("./mixkit-classic-alarm-995.wav")
     return
 
@@ -29,28 +29,35 @@ async def caffeinate(finished):
 
 async def main():
     while True:
-        timer = input("Timer duration hh:mm:ss : ")
-        time_format = re.compile("\d\d:\d\d:\d\d")
-        if time_format.match(timer) is None:
-            print("Bad format")
-            continue
-        dd = re.compile("\d\d")
-        h, m, s = [int(d) for d in dd.findall(timer)]
-        seconds = h * 60 * 60 + m * 60 + s
+        timer = input("Presets [1]: 20min, [2]: 1h, or Timer duration hh:mm:ss : ")
+        match timer:
+            case "1":
+                h, m, s = 0, 20, 0
+                seconds = 20 *60
+            case "2":
+                seconds = 60 * 60
+                h, m, s = 1, 0, 0
+            case _:
+                time_format = re.compile("\d\d:\d\d:\d\d")
+                if time_format.match(timer) is None:
+                    print("Bad format")
+                    continue
+                dd = re.compile("\d\d")
+                h, m, s = [int(d) for d in dd.findall(timer)]
+                seconds = h * 60 * 60 + m * 60 + s
         print(
             f"setting timer for {h} h, {m} m, {s} s (total {seconds} seconds)"
         )
-        try:
-            finished = asyncio.Event()
-            await asyncio.gather(
-                asyncio.wait_for(caffeinate(finished), timeout=seconds),
-                set_timer(seconds)
-            )
-            await finished.wait()
-        except KeyboardInterrupt:
-            print("Interrupting")
-            return
+        finished = asyncio.Event()
+        await asyncio.gather(
+            asyncio.wait_for(caffeinate(finished), timeout=seconds),
+            set_timer(seconds)
+        )
+        await finished.wait()
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        pass
